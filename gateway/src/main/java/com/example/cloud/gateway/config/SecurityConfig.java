87185999b99d63cf36b71d5f9b5d94d6ab480774
Vadim Sartakov <sartakov.vadim@yandex.ru>
2018-06-17 00:15:48 +0500

    Example enhancing.

---
+++ gateway/src/main/java/com/example/cloud/gateway/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.example.cloud.gateway.config;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableOAuth2Sso
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/auth/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .formLogin().and()
+                .csrf().ignoringAntMatchers(/*"/auth/login", */"/auth/oauth/token");
+    }
+    
+}
---
+++ resource-server/src/main/java/com/example/cloud/resourceserver/Oauth2ResourceApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloud.resourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@SpringBootApplication
+@EnableResourceServer
+public class Oauth2ResourceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2ResourceApplication.class, args);
+    }
+}
