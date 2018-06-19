1458ecb528c5f85fcf34c0b98575c1277d677414
Vadim Sartakov <sartakov.vadim@yandex.ru>
2018-06-19 17:21:03 +0500

    Minor fixes.

--- auth-server/src/main/java/com/example/cloud/authserver/config/SecurityConfig.java
+++ auth-server/src/main/java/com/example/cloud/authserver/config/SecurityConfig.java
@@ -3,10 +3,8 @@ package com.example.cloud.authserver.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @Configuration
-@EnableResourceServer
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     
     @Override
--- gateway/src/main/java/com/example/cloud/gateway/config/SecurityConfig.java
+++ gateway/src/main/java/com/example/cloud/gateway/config/SecurityConfig.java
@@ -1,37 +1,22 @@
 package com.example.cloud.gateway.config;
 
-import com.example.cloud.gateway.filter.JWTAuthorizationFilter;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 @Configuration
-@EnableOAuth2Sso
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableResourceServer
+public class SecurityConfig extends ResourceServerConfigurerAdapter {
            
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    public void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
                     .antMatchers("/auth/**")
                     .permitAll()
                     .anyRequest()
-                    .authenticated()
-                /*.and()
-                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)*/
-                .and()
-                    .formLogin()
-                .and()
-                    .csrf().disable()
-                    .addFilter(jwtAuthorizationFilter());
-    }
-
-    @Bean
-    public JWTAuthorizationFilter jwtAuthorizationFilter() throws Exception {
-        return new JWTAuthorizationFilter(authenticationManager());
+                    .authenticated();
     }
     
 }
