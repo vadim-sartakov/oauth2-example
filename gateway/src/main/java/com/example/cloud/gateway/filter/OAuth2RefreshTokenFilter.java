70ba4206d867e8cbd6a7c3b52aa82419731d1f34
Vadim Sartakov <sartakov.vadim@yandex.ru>
2018-06-25 18:01:47 +0500

    OAuth2 refresh token filter.

--- gateway/src/main/java/com/example/cloud/gateway/filter/OAuth2RefreshTokenFilter.java
+++ gateway/src/main/java/com/example/cloud/gateway/filter/OAuth2RefreshTokenFilter.java
@@ -1,46 +1,77 @@
 package com.example.cloud.gateway.filter;
 
 import java.io.IOException;
+import java.util.Arrays;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.token.AccessTokenRequest;
+import org.springframework.security.oauth2.client.token.DefaultAccessTokenRequest;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeAccessTokenProvider;
 import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.common.exceptions.OAuth2Exception;
 import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.util.WebUtils;
 
 public class OAuth2RefreshTokenFilter extends GenericFilterBean {
 
     @Autowired private AuthorizationCodeResourceDetails resourceDetails;
     @Autowired private OAuth2ClientContext clientContext;
-    
-    private OAuth2RestTemplate restTemplate;
+    @Autowired private TokenStore tokenStore;
     
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        
         try {
             chain.doFilter(request, response);
         } catch(BadCredentialsException bad) {
+            
             Throwable cause = bad.getCause();
             if (cause instanceof OAuth2Exception) {
+                
                 OAuth2Exception oauth2Cause = (OAuth2Exception) cause;
                 if (oauth2Cause.getMessage().contains("expired")) {
-                    restTemplate.getAccessToken();
+                    
+                    Cookie cookie = WebUtils.getCookie(httpRequest, OAuth2AccessToken.REFRESH_TOKEN);
+                    if (cookie != null) {
+                        
+                        AuthorizationCodeAccessTokenProvider provider = new AuthorizationCodeAccessTokenProvider();
+                        AccessTokenRequest tokenRequest = new DefaultAccessTokenRequest();
+                        OAuth2AccessToken accessToken = provider.refreshAccessToken(resourceDetails, tokenStore.readRefreshToken(cookie.getValue()), tokenRequest);
+                        clientContext.setAccessToken(accessToken);
+                        
+                        request = new HttpServletRequestWrapper(httpRequest) {
+                            @Override
+                            public Cookie[] getCookies() {
+                                Cookie[] cookies = super.getCookies();
+                                Arrays.asList(cookies).stream()
+                                        .filter(cookie -> cookie.getName().equals(OAuth2AccessToken.ACCESS_TOKEN))
+                                        .findFirst()
+                                        .ifPresent(cookie -> cookie.setValue(accessToken.getValue()));
+                                return cookies;
+                            }
+                            
+                        };
+                        
+                    }
+    
                 }
             }
-            chain.doFilter(request, response);
         }
+        chain.doFilter(request, response);
     }
 
-    @Override
-    public void afterPropertiesSet() throws ServletException {
-        super.afterPropertiesSet();
-        restTemplate = new OAuth2RestTemplate(resourceDetails, clientContext);
-    }
-    
 }
